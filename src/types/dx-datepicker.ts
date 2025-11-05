e515d81ba0de559b86929230766a19577d268f4e
Hanjo Hagemeier <hanjo.hagemeier@hcl.com>
2025-11-05 09:59:08 +0100

    set the correct copyrights

--- src/types/dx-datepicker.ts
+++ src/types/dx-datepicker.ts
@@ -1,14 +1,17 @@
-/*
- ********************************************************************
- * Licensed Materials - Property of HCL                             *
- *                                                                  *
- * Copyright HCL Technologies Ltd. 2025. All Rights Reserved.       *
- *                                                                  *
- * Note to US Government Users Restricted Rights:                   *
- *                                                                  *
- * Use, duplication or disclosure restricted by GSA ADP Schedule    *
- ********************************************************************
- */
+/* ======================================================================== *
+ * Copyright 2025 HCL America Inc.                                          *
+ * Licensed under the Apache License, Version 2.0 (the "License");          *
+ * you may not use this file except in compliance with the License.         *
+ * You may obtain a copy of the License at                                  *
+ *                                                                          *
+ * http://www.apache.org/licenses/LICENSE-2.0                               *
+ *                                                                          *
+ * Unless required by applicable law or agreed to in writing, software      *
+ * distributed under the License is distributed on an "AS IS" BASIS,        *
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ * See the License for the specific language governing permissions and      *
+ * limitations under the License.                                           *
+ * ======================================================================== */
 
 export const MIN_YEAR = 1900; // Since January 1, 1900 is the minimum date for most datepicker implementations
 export const MAX_YEAR_BUFFER = 100; // Buffer to allow for future years, e.g., 100 years from the current year
